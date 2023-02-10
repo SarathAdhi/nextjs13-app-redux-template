8e8c13cee062292c442d0590fc0e01d8ae1744a9
CodeSandbox <support@codesandbox.io>
2023-02-10 05:15:44 +0000

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { UserDetails } from "./(components)/UserDetails";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>User Details</h1>
+      <UserDetails />
+    </div>
+  );
+}
